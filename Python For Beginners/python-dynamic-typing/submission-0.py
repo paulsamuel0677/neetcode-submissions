aeddc77d40c2167deef35fe1fd681ffcd7216d8c
Paul Samuel M <paulsamuel0677@gmail.com>
2026-06-21 13:22:59 +0530

    Add: python-dynamic-typing - submission-0

---
+++ Python For Beginners/python-dynamic-typing/submission-0.py
@@ -0,0 +1,10 @@
+variable = 103
+print(type(variable))
+variable = 324.342
+print(type(variable))
+variable = True
+print(type(variable))
+variable = "Shock"
+print(type(variable))
+variable = [1,2,3,45,2]
+print(type(variable))
